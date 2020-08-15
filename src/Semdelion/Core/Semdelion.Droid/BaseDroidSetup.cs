316f749364d2a6122978a1c205aed1077b71ba41
n.pashkova <neldnelaps@gmail.com>
2020-08-15 16:41:55 +0700

    Added Localization

---
+++ src/Semdelion/Core/Semdelion.Droid/BaseDroidSetup.cs
@@ -0,0 +1,52 @@
+﻿namespace Semdelion.Droid
+{
+    using Android.Views;
+    using MvvmCross.Binding.Bindings.Target.Construction;
+    using MvvmCross.Converters;
+    using MvvmCross.Droid.Support.V7.AppCompat;
+    using MvvmCross.Localization;
+    using MvvmCross.Platforms.Android.Core;
+    using MvvmCross.Platforms.Android.Presenters;
+    using MvvmCross.ViewModels;
+    using Semdelion.Core;
+    using Semdelion.Droid.Bindings;
+    using System.Globalization;
+
+    public abstract class BaseDroidSetup : MvxAndroidSetup
+    {
+        private App _app;
+
+        protected override IMvxAndroidViewPresenter CreateViewPresenter()
+        {
+            return new MvxAppCompatViewPresenter(this.AndroidViewAssemblies);
+        }
+
+        protected override void FillTargetFactories(IMvxTargetBindingFactoryRegistry registry)
+        {
+            MvxAppCompatSetupHelper.FillTargetFactories(registry);
+            base.FillTargetFactories(registry);
+            registry.RegisterCustomBindingFactory<ViewGroup>(StatesTargetBinding.Key,
+                viewGroup => new StatesTargetBinding(viewGroup));
+        }
+
+        protected override void FillValueConverters(IMvxValueConverterRegistry registry)
+        {
+            base.FillValueConverters(registry);
+            registry.AddOrOverwrite("Language", new MvxLanguageConverter());
+        }
+
+        protected sealed override IMvxApplication CreateApp()
+        {
+            _app = CreateSemApp();
+            return _app;
+        }
+
+        protected abstract App CreateSemApp();
+
+        public override void InitializeSecondary()
+        {
+            base.InitializeSecondary();
+            _app.InitializeCultureInfo(new CultureInfo("ru-RU"));
+        }
+    }
+}
--- src/Semdelion/Playground/Playground.Droid/Setup.cs
+++ src/Semdelion/Playground/Playground.Droid/Setup.cs
@@ -1,27 +1,11 @@
-﻿using Android.Views;
-using MvvmCross.Binding.Bindings.Target.Construction;
-using MvvmCross.Droid.Support.V7.AppCompat;
-using MvvmCross.Platforms.Android.Core;
-using MvvmCross.Platforms.Android.Presenters;
-using Playground.Core;
-using Semdelion.Droid.Bindings;
+﻿using Playground.Core;
+using Semdelion.Core;
+using Semdelion.Droid;
 
 namespace Playground.Droid
 {
-    public class Setup : MvxAndroidSetup <PlaygroundApp>
+    public class Setup : BaseDroidSetup// MvxAndroidSetup <PlaygroundApp>
     {
-
-        protected override IMvxAndroidViewPresenter CreateViewPresenter()
-        {
-            return new MvxAppCompatViewPresenter(this.AndroidViewAssemblies);
-        }
-
-		protected override void FillTargetFactories(IMvxTargetBindingFactoryRegistry registry)
-		{
-			MvxAppCompatSetupHelper.FillTargetFactories(registry);
-			base.FillTargetFactories(registry);
-			registry.RegisterCustomBindingFactory<ViewGroup>(StatesTargetBinding.Key,
-				viewGroup => new StatesTargetBinding(viewGroup));
-		}
-	}
+        protected override App CreateSemApp() => new PlaygroundApp();
+    }
 }
